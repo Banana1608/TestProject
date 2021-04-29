9595edc4216ef2d747dac3182d89af246f66c5b6
bbb <bbb@mail.com>
2021-04-29 19:23:07 +0300

    Commit before clone

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		System.out.println("Hello World!!!");
 		System.out.println("Second line!!");
 		System.out.println("Add line!!");
+		System.out.println("Add second line!!");
+		
 	}
 
 }
